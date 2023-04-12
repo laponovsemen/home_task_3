d1a2ca90d99d4eb7016bbadf07ee17c991c3d206
Laponov Semen <johndoe@example.com>
2023-04-12 23:00:44 +0300

    adding delete all data

--- src/blogs/blogsRouter.ts
+++ src/blogs/blogsRouter.ts
@@ -2,16 +2,16 @@ import {Router} from 'express'
 import {createBlog, deleteBlogById, getAllBlogs, getBlogById, updateBlog} from "./blogsRepositoryMongoDB";
 import {
     BlogDescriptionValidation,
-    BlogNameValidation, BlogValidationErrors,
+    BlogNameValidation,
     BlogWebsiteUrlValidation,
     CreateBlogValidation
 } from "./blogValidators";
-import {basicAuthGuardMiddleware} from "../common";
+import {basicAuthGuardMiddleware, ValidationErrors} from "../common";
 
 export const blogsRouter = Router({})
 
 blogsRouter.get("", getAllBlogs)
-blogsRouter.post("", basicAuthGuardMiddleware,BlogNameValidation, BlogDescriptionValidation, BlogWebsiteUrlValidation, BlogValidationErrors, createBlog)
+blogsRouter.post("", basicAuthGuardMiddleware,BlogNameValidation, BlogDescriptionValidation, BlogWebsiteUrlValidation, ValidationErrors, createBlog)
 blogsRouter.get("/:id", getBlogById)
 blogsRouter.put("/:id",basicAuthGuardMiddleware, updateBlog)
 blogsRouter.delete("/:id",basicAuthGuardMiddleware,  deleteBlogById)
